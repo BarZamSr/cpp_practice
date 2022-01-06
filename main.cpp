16928c24650625a590a9f3c257bf2bd6781f9a2c
BarZamSr <barzamsr@gmail.com>
2022-01-06 17:16:00 +1100

    nothing

--- main.cpp
+++ main.cpp
@@ -1,26 +1,9 @@
 #include <iostream>
 
+#include "utils.cpp"
 #include "list.cpp"
-
-class Test {
-public:
-	Test() {
-		std::cout << '+' << ++id << std::endl;
-	}
-	~Test() {
-		std::cout << '-' << id << std::endl;
-	}
-private:
-	static int id;
-};
-
-Test function() {
-	Test * retval = new Test();
-	return * retval;
-}
+#include "string.cpp"
 
 int main() {
-	// std::cout << List<int>({1, 2, 3}) * 2 << std::endl;
-	
 	return 0;
 }
--- string.cpp
+++ string.cpp
@@ -3,14 +3,69 @@
 
 #include "list.cpp"
 
-class String: List<char> {
+class String: public List<char> {
+public:
+	String(const char * c_str): List<char>(c_str, utils::c_str_len(c_str)) {
+		// hmmm
+	}
+	String(const char * A, int n): List<char>(A, n) {
+		// hmmm
+	}
 
-};
+	String operator+ (const String & other) const {
+		String sum(*this);
+		sum += other;
+		return sum;
+	}
 /*
-String::String(char const* c_str): String(utils::c_str_len(c_str)) {
-	len = cap;
-	utils::copy(c_str, array, cap);
-}
+	int find(const String & sub_str) {
+		return find(sub_str, 0, len);
+	}
+	int find(const String & sub_str, int start) {
+		return find(sub_str, start, len);
+	}
+	int find(const String & sub_str, int start, int end) {
+		LOG("find(String)");
+		for(int i=0; i<len; i++) {
+			for(int j=0; i+j<len; j++) {
+				if(j == sub_str.len) return i;
+				if(array[i+j] != sub_str[j]) break;
+			}
+		}
+		return ERR_VAL;
+	}
 */
+	List<String> split(char c = ' ') {
+		List<String> sub_strings;
+
+		int curr = find(c), last=0;
+		while(curr != ERR_VAL) {
+			sub_strings.push(
+				String(array+last, curr-last)
+			);
+
+			last = curr+1;
+			curr = find(c, last);
+		}
+		sub_strings.push(
+			String(array+last, len-last)
+		);
+
+		return sub_strings;
+	}
+
+	friend std::ostream & operator<< (std::ostream & stream,
+						const String & string) {
+		for(const char & c: string) {
+			stream << c;
+		}
+		return stream;
+	}
+
+private:
+	String(int n): List<char>(n) {
+		// hmmm
+	}
+};
 
 #endif
